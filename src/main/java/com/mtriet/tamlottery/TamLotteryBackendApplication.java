4a84883173ea461be385087ed81c5fcba01dd226
mtriet004 <triet172004@gmail.com>
2026-07-17 18:20:18 +0700

    first commit

---
+++ src/main/java/com/mtriet/tamlottery/TamLotteryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mtriet.tamlottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamLotteryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamLotteryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtriet/tamlottery/TamLotteryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtriet.tamlottery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamLotteryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
